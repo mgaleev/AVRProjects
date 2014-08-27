0a23148e8fd9978255a8eb0182584f402fd46016
Mikhail Galeev <mikhail.galeev@iscointl.com>
2014-08-27 09:23:42 -0500

    Adding source and project files

---
+++ SPIBridge/spi_shortcut_sparkfun/spi_shortcut_sparkfun.c
@@ -0,0 +1,17 @@
+/*
+ * spi_shortcut_sparkfun.c
+ *
+ * Created: 8/22/2014 2:00:53 PM
+ *  Author: MGaleev
+ */ 
+
+
+#include <avr/io.h>
+
+//int main(void)
+//{
+//    while(1)
+//    {
+//        //TODO:: Please write your application code 
+//    }
+//}
